da9f1240dbb0a4a5753f8d6051c0f57d4fffc984
jelhan <jeldrik@systemli.org>
2014-08-03 17:19:11 +0200

    Fix wrong tab

--- app/views/poll.js
+++ app/views/poll.js
@@ -2,7 +2,7 @@ export default Ember.View.extend({
   didInsertElement : function(){
     this._super();
     Ember.run.scheduleOnce('afterRender', this, function(){
-    	$('.user-selections-table').floatThead({});
+      $('.user-selections-table').floatThead({});
     });
   }
 });
