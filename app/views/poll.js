8014e6e96532d11ef072c4d5adf522fdd4554eb0
jelhan <jeldrik@systemli.org>
2014-08-03 17:12:42 +0200

    floating thead of user-selection-table

---
+++ app/views/poll.js
@@ -0,0 +1,8 @@
+export default Ember.View.extend({
+  didInsertElement : function(){
+    this._super();
+    Ember.run.scheduleOnce('afterRender', this, function(){
+    	$('.user-selections-table').floatThead({});
+    });
+  }
+});
