fcfb3f89f144bc3351020935026d3ac36b27006f
jelhan <jeldrik@systemli.org>
2014-10-25 00:59:06 +0200

    show day of the week
    Closes #22

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -1,3 +1,10 @@
-export default function(date, format) {
-  return moment(date).format(format);
+export default function(date, options) {
+  var times = options.hash.times ? options.hash.times : false,
+      format = options.hash.format ? options.hash.format : 'LLLL';
+  if (times === true) {
+    return moment(date).format( format );
+  }
+  else {
+    return moment(date).format(moment.localeData().longDateFormat( format ).replace('LT' , ''));
+  }
 }
