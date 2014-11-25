baa048f0df3e519e7974d307d42f1536eb6b23d6
jelhan <jeldrik@systemli.org>
2014-11-25 15:44:56 +0100

    back button: load old selection into calendar

--- app/views/datepicker.js
+++ app/views/datepicker.js
@@ -4,7 +4,8 @@ export default Ember.View.extend({
   classNames: ['datepicker'],
 
   didInsertElement: function() {
-    var self = this;
+    var self = this,
+        selectedDates = [];
 
     this._super();
 
@@ -16,7 +17,7 @@ export default Ember.View.extend({
       todayHighlight: true,
       language: this.get('controller.language.selected')
     })
-    // bind date changes to option array in model
+    // bind date changes to dates option array in model
     .on('changeDate', function(e){
       var dates = e.dates,
           newDates = [];
@@ -34,5 +35,16 @@ export default Ember.View.extend({
       // set options
       self.set('_parentView.controller.optionsDates', newDates);
     });
+    
+    // get selected dates in a simple array of date objects
+    this.get('controller.optionsDates').forEach(function(date){
+      console.log(date.title);
+      selectedDates.push( new Date(date.title) );
+    });
+    
+    if(selectedDates.length > 0) {
+      // set selected dates on init
+      Ember.$('#' + this.elementId).datepicker('setDates', selectedDates);
+    }
   }
 });
