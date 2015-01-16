4b2f287e8fc5a1d74e173e3ba70dfb211aa6e0ed
jelhan <jeldrik@systemli.org>
2015-01-16 16:38:47 +0100

    fix content security policy issue
    Should remove 'script-src' 'unsafe-eval' in future for better security.
    It's required by modenizr.

--- config/environment.js
+++ config/environment.js
@@ -14,6 +14,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+    
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-eval'",
+      'font-src': "'self'",
+      'connect-src': "'self'",
+      'img-src': "'self'",
+      'style-src': "'self' 'unsafe-inline'",
+      'media-src': "'self'"
     }
   };
 
