1c831870e43dcf10b5ac4f4c04fb32b2617cda33
jelhan <jeldrik@systemli.org>
2015-01-19 23:37:51 +0100

    use ember-radio-button, an ember cli adddon, for radio buttons

--- app/helpers/radio-button.js
+++
@@ -1,8 +0,0 @@
-import Ember from "ember";
-
-/*
- * radio button helper by FellowMD
- * https://gist.github.com/FellowMD/84f5b113de2db762cb1d
- */
-import RadioButtonView from '../views/radio-button';
-export default Ember.Handlebars.makeViewHelper(RadioButtonView);
