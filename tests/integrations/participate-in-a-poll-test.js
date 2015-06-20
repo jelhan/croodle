5cce0a236d38cad06513b148b46f6f833ca986f4
jelhan <jeldrik@systemli.org>
2015-06-20 18:33:51 +0200

    Fix: freetext answer where broken by caba42a85362746cee909adf4ed1b1f5f1181a5e added test to not let it happen again

--- tests/integrations/participate-in-a-poll-test.js
+++ tests/integrations/participate-in-a-poll-test.js
@@ -39,3 +39,28 @@ test("add a user to a default poll", function(assert) {
     });
   });
 });
+
+test("participate in a poll using freetext", function(assert) {
+  assert.expect(6);
+
+  visit('/poll/freetext?encryptionKey=JU8dqNCJivwqfRpP28M5gpcqK0BsfgjUkDoXvTTX').then(function() {
+    fillIn('.newUserName input', 'Karl Käfer');
+    
+    fillIn('.newUserSelection input:eq(0)', 'answer 1');
+    fillIn('.newUserSelection input:eq(1)', 'answer 2');
+    fillIn('.newUserSelection input:eq(2)', 'answer 3');
+    
+    var userSelecectionsTableLengthBefore = Ember.$('.user').length;
+    click('.newUser button');
+   
+    assert.equal(Ember.$('.has-error').length, 0, "there is no validation error");
+ 
+    andThen(function(){
+      assert.equal( find('.user').length, userSelecectionsTableLengthBefore + 1, "user is added to user selections table");
+      assert.equal( find('.user td:nth-child(1)').text(), 'Karl Käfer', "user name is correct");
+      assert.equal( find('.user td:nth-child(2)').text().trim(), 'answer 1', "answer 1 is correct");
+      assert.equal( find('.user td:nth-child(3)').text().trim(), 'answer 2', "answer 2 is correct");
+      assert.equal( find('.user td:nth-child(4)').text().trim(), 'answer 3', "answer 3 is correct");
+    });
+  });
+});

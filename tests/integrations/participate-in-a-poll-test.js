jelhan/croodle

commit caba42a85362746cee909adf4ed1b1f5f1181a5e

using ember-cli-easyform-extensions as it support ember >= 1.10

app/templates/poll.hbs
@@ -66,7 +66,7 @@
       <tbody>
         <tr class='newUser'>
           <td>
-            {{#form-for controller}}
+            {{#form-wrapper}}
               {{#input newUserName}}
                 {{input-field newUserName
                     placeholderTranslation="poll.input.newUserName.placeholder"
@@ -75,14 +75,14 @@
                   {{error-field newUserName}}
                 {{/if}}
               {{/input}}
-            {{/form-for}}
+            {{/form-wrapper}}
           </td>
           {{#each newUserSelection in controller.newUserSelections}}
             <td class="newUserSelection">
-              {{#form-for newUserSelection}}
+              {{#form-wrapper}}
                 {{#if isFreeText}}
-                  {{#input value}}
-                    {{input-field value}}
+                  {{#input newUserName.value}}
+                    {{input-field newUserName.value}}
                   {{/input}}
                 {{else}}
                   {{#each answer in answers}}
@@ -96,7 +96,7 @@
                     </div>
                   {{/each}}
                 {{/if}}
-              {{/form-for}}
+              {{/form-wrapper}}
             </td>
           {{/each}}
           <td>

Brocfile.js
@@ -51,8 +51,6 @@ var bootstrapFonts = pickFiles('bower_components/bootstrap/dist/fonts', {
 app.import('bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js');
 app.import('bower_components/bootstrap-datepicker/js/locales/bootstrap-datepicker.de.js');
 
-app.import('bower_components/ember-easyForm/index.js');
-
 app.import('bower_components/ember-i18n/lib/i18n.js');
 app.import('bower_components/ember-i18n/lib/i18n-plurals.js');
 

app/templates/create/index.hbs
@@ -1,5 +1,5 @@
 <div class="box">
-  {{#form-for controller}}
+  {{#form-wrapper}}
     {{input pollType as='select'
         collection="pollTypes"
         value="pollType"
@@ -7,7 +7,7 @@
         optionLabelPath="content.label"
         labelTranslation='create.index.input.pollType.label'
     }}
-  {{/form-for}}
+  {{/form-wrapper}}
 
   <button {{action "submit"}} class="btn btn-default btn-primary button-next"> {{t 'create.next'}} </button>
 </div>

app/templates/create/meta.hbs
@@ -1,5 +1,5 @@
 <div class="box">
-  {{#form-for controller}}
+  {{#form-wrapper}}
     {{input title
         labelTranslation='create.meta.input.title.label'
         placeholderTranslation="create.meta.input.title.placeholder"
@@ -9,7 +9,7 @@
         placeholderTranslation="create.meta.input.description.placeholder"
         rows=4
     }}
-  {{/form-for}}
+  {{/form-wrapper}}
 
   <button {{action "submit"}} class="btn btn-default btn-primary button-next"> {{t 'create.next'}} </button>
 

app/templates/create/options-datetime.hbs
@@ -1,5 +1,5 @@
 <div class="box">
-  {{#form-for controller}}
+  {{#form-wrapper controller}}
     {{#input enoughTimes}}
       {{label-field enoughTimes
           textTranslation="create.options-datetime.datetimes.label"
@@ -30,7 +30,7 @@
     {{#input correctTimeFormat}}
       {{error-field correctTimeFormat}}
     {{/input}}
-  {{/form-for}}
+  {{/form-wrapper}}
 
   <button {{action "moreTimes"}} class="btn btn-default button-more-times">{{t "create.options-datetime.more-inputs"}}</button>
 

app/templates/create/options.hbs
@@ -1,7 +1,7 @@
 <div class="box">
   {{#if isMakeAPoll}}
 
-    {{#form-for controller}}
+    {{#form-wrapper}}
       
       {{#input enoughOptions}}
         {{label-field enoughOptions
@@ -16,7 +16,7 @@
           {{error-field enoughOptions}}
         {{/if}}
       {{/input}}
-    {{/form-for}}
+    {{/form-wrapper}}
  
     <button {{action "moreOptions" target="view"}} class="btn btn-default button-more-options"> {{t 'create.options.input.moreOptions.label'}} </button>
 
@@ -30,7 +30,7 @@
       {{view 'datepicker'}}
     </div>
       
-    {{#form-for controller}}
+    {{#form-wrapper}}
       {{#input enoughOptions}}
         {{#if view.showError}}
           {{error-field enoughOptions}}
@@ -40,7 +40,7 @@
       {{input isDateTime as='checkbox'
          labelTranslation="create.options.defineTimes.label"
       }}
-    {{/form-for}}
+    {{/form-wrapper}}
     
     <button {{action "submit"}} class="btn btn-default btn-primary button-next"> {{t 'create.next'}} </button>
 

app/templates/create/settings.hbs
@@ -1,5 +1,5 @@
 <div class="box">
-  {{#form-for controller}}
+  {{#form-wrapper}}
     {{input answerType as='select'
       collection="answerTypes"
       value="answerType"
@@ -13,7 +13,7 @@
     {{input forceAnswer as='checkbox'
       labelTranslation="create.settings.forceAnswer.label"
     }}
-  {{/form-for}}
+  {{/form-wrapper}}
 
   <button {{action "submit"}} class="btn btn-default btn-primary button-next"> {{t 'create.next'}} </button>
 </div>

commit 5cce0a236d38cad06513b148b46f6f833ca986f4

Fix: freetext answer where broken by caba42a85362746cee909adf4ed1b1f5f1181a5e added test to not let it happen again

app/templates/poll.hbs
@@ -80,8 +80,8 @@
               <td class="newUserSelection">
                 {{#form-wrapper}}
                   {{#if isFreeText}}
-                    {{#input newUserName.value}}
-                      {{input-field newUserName.value}}
+                    {{#input newUserSelection.value}}
+                      {{input-field newUserSelection.value}}
                     {{/input}}
                   {{else}}
                     {{#each answer in answers}}

server/dummy/freetext/poll_data
@@ -1,0 +1,1 @@
+{"poll":{"encryptedTitle":"{\"iv\":\"+B4FlaIOKKI0xIXCe8ctYA==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"jJNiKc43db+/q83pscM8AWxUMzFsZkeFcj9sH/gBlOXwRbNouXoM\"}","encryptedDescription":"{\"iv\":\"nA4zgtTCcxha7UejHbHV+Q==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"eEdAX+xFzawEPw==\"}","encryptedPollType":"{\"iv\":\"2soA8c0P9euzRncyV+Tvaw==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"Up6cJY/OKkmOuY+VYDC/H4MgRw==\"}","encryptedAnswerType":"{\"iv\":\"j35s9U5BzBg6ML9kshtvfg==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"FeYT5TOF8M2u+tnsJoeYMKfV\"}","encryptedAnswers":"{\"iv\":\"UHOV6M15JAHbsQP2BnqgYg==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"KsPARhgLPrcPPw==\"}","encryptedOptions":"{\"iv\":\"f9HCMoqTSQuDG7cBBdm0iw==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"8WGoESCChni0zsV0IRNqTP6odGOEyDB69SxcdOMgfc2S+BiBjzpB6dWa0ar8SeV44XJg1TWxtgmBGKWpQxYBTYFUm0Zf/Kg1\"}","encryptedCreationDate":"{\"iv\":\"TTWX5d4C6rlrCBKj5I6A8Q==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"QKvoPA52bljeqfoK4vZUNApYYIriA8602kD2SyWTH4m4FQ==\"}","encryptedForceAnswer":"{\"iv\":\"hm+f/t52R3gqkOdZwULisg==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"kvLq90xQl2VUv5fG\"}","encryptedAnonymousUser":"{\"iv\":\"VKBCAAVPnBpE5ze1wPWaOg==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"yfHql5FZ/k+2rybL0A==\"}","encryptedIsDateTime":null,"encryptedTimezoneOffset":"{\"iv\":\"MUUD9BA2h+aBlNn9GG0lMQ==\",\"v\":1,\"iter\":1000,\"ks\":128,\"ts\":64,\"mode\":\"ccm\",\"adata\":\"\",\"cipher\":\"aes\",\"salt\":\"h9FzrJ9siNY=\",\"ct\":\"D6VGY5oqFRA4eHNe\"}"}}

tests/integrations/participate-in-a-poll-test.js
@@ -39,3 +39,28 @@ test("add a user to a default poll", function(assert) {
     });
   });
 });
+
+test("participate in a poll using freetext", function(assert) {
+  assert.expect(6);
+
+  visit('/poll/freetext?encryptionKey=JU8dqNCJivwqfRpP28M5gpcqK0BsfgjUkDoXvTTX').then(function() {
+    fillIn('.newUserName input', 'Karl Käfer');
+    
+    fillIn('.newUserSelection input:eq(0)', 'answer 1');
+    fillIn('.newUserSelection input:eq(1)', 'answer 2');
+    fillIn('.newUserSelection input:eq(2)', 'answer 3');
+    
+    var userSelecectionsTableLengthBefore = Ember.$('.user').length;
+    click('.newUser button');
+   
+    assert.equal(Ember.$('.has-error').length, 0, "there is no validation error");
+ 
+    andThen(function(){
+      assert.equal( find('.user').length, userSelecectionsTableLengthBefore + 1, "user is added to user selections table");
+      assert.equal( find('.user td:nth-child(1)').text(), 'Karl Käfer', "user name is correct");
+      assert.equal( find('.user td:nth-child(2)').text().trim(), 'answer 1', "answer 1 is correct");
+      assert.equal( find('.user td:nth-child(3)').text().trim(), 'answer 2', "answer 2 is correct");
+      assert.equal( find('.user td:nth-child(4)').text().trim(), 'answer 3', "answer 3 is correct");
+    });
+  });
+});