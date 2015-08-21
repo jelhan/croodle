0ae62f31f1320771fae14852193fd88c88dd9c5e
jelhan <jeldrik@systemli.org>
2015-08-21 12:29:09 +0200

    added test for translation object

---
+++ tests/unit/lang-test.js
@@ -0,0 +1,29 @@
+import languages from '../../lang/translations';
+import { module, test } from 'qunit';
+
+module('Unit | languages');
+
+// Replace this with your real tests.
+test('translations are correct', function(assert) {
+  assert.ok(languages);
+  Object.keys(languages).map(function(language) {
+    if (language !== 'en') {
+      assert.equal(
+        Object.keys(languages[language]).length,
+        Object.keys(languages.en).length,
+        'language ' + language + ' has same amount of translation keys as english language'
+      );
+    }
+  });
+  
+  Object.keys(languages.en).map(function(translationKey) {
+    Object.keys(languages).map(function(language) {
+      if (language !== 'en') {
+        assert.ok(
+          languages[language][translationKey],
+          'translation with key ' + translationKey + ' exist for language ' + language
+        );
+      }
+    });
+  });
+});
