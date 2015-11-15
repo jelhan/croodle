8cb8b9bf6a0f02f1a9fabfd7bc7b6c1bfa8b496d
jelhan <jeldrik@systemli.org>
2015-11-15 19:22:34 +0100

    fix jshint

--- app/components/fm-submit.js
+++ app/components/fm-submit.js
@@ -1,4 +1,6 @@
 import FmSubmitComponent from 'ember-form-master-2000/components/fm-submit';
+import Ember from 'ember';
+
 export default FmSubmitComponent.reopen({
   fieldWrapperClass: Ember.computed(function() {
     return this.fmconfig.fieldWrapperClass;
