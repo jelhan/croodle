02053d6c38801349d7e8e7e6c7d9137613f96ae4
jelhan <jeldrik@systemli.org>
2016-01-29 11:24:43 +0100

    some more code style

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -7,13 +7,12 @@ export default function(date, options) {
   if (options.hash === undefined) {
     options.hash = {};
   }
-  
-  var times = options.hash.times ? options.hash.times : false,
-      format = options.hash.format ? options.hash.format : 'LLLL';
+
+  const times = options.hash.times ? options.hash.times : false;
+  const format = options.hash.format ? options.hash.format : 'LLLL';
   if (times === true) {
-    return moment(date).format( format );
-  }
-  else {
+    return moment(date).format(format);
+  } else {
     return moment(date).format(
       moment.localeData().longDateFormat(format)
       .replace(
