014c717d34f818a9305cc2952b6b6542bf6a85db
jelhan <jeldrik@systemli.org>
2016-02-18 12:06:54 +0100

    sort options after times where set

--- app/controllers/create/options-datetime.js
+++ app/controllers/create/options-datetime.js
@@ -10,12 +10,11 @@ export default Ember.Controller.extend({
     }
   },
 
-  /*
-   * removes all days from options which haven't a time but there is atleast
-   * one option with time for that day
-   */
   normalizeOptions() {
     const options = this.get('options');
+
+    // remove all days from options which haven't a time but there is atleast
+    // one option with time for that day
     const daysWithTime = options.map((option) => {
       if (moment(option.get('title'), 'YYYY-MM-DD', true).isValid()) {
         return null;
@@ -29,6 +28,10 @@ export default Ember.Controller.extend({
     options.removeObjects(
       removeObjects
     );
+
+    // sort options
+    // ToDo: Find a better way without reseting the options
+    this.set('options', options.sortBy('title'));
   },
   isDateTime: Ember.computed.alias('model.isDateTime'),
   options: Ember.computed.alias('model.options')
--- tests/unit/controllers/create/options-datetime-test.js
+++ tests/unit/controllers/create/options-datetime-test.js
@@ -1,10 +1,11 @@
 import { moduleFor, test } from 'ember-qunit';
 import Ember from 'ember';
+import moment from 'moment';
 
 moduleFor('controller:create/options-datetime', 'Unit | Controller | create/options datetime', {
 });
 
-test('normalize options', function(assert) {
+test('normalize options - remove days without time if there is another option with a time for that day', function(assert) {
   const dirtyOption = Ember.Object.create({ title: '2015-01-01' });
   let controller = this.subject({
     model: {
@@ -27,5 +28,30 @@ test('normalize options', function(assert) {
   assert.ok(
     controller.get('options').indexOf(dirtyOption) === -1,
     'correct option is removed'
+  );
+});
+
+test('normalize options - sort them', function(assert) {
+  const dateA = moment().hour(5).toISOString();
+  const dateB = moment().hour(10).toISOString();
+  const dateC = moment().hour(22).toISOString();
+  const dateD = moment().add(1, 'day').toISOString();
+  let controller = this.subject({
+    model: {
+      options: [
+        Ember.Object.create({ title: dateB }),
+        Ember.Object.create({ title: dateA }),
+        Ember.Object.create({ title: dateC }),
+        Ember.Object.create({ title: dateD })
+      ]
+    }
+  });
+  Ember.run(() => {
+    controller.normalizeOptions();
+  });
+  assert.deepEqual(
+    controller.get('options').map((option) => option.get('title')),
+    [dateA, dateB, dateC, dateD],
+    'options are sorted'
   );
 });
