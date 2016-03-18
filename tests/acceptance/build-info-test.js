9a658ab76da82c8b7ff1ca2174c583de5a055acd
jelhan <jeldrik@systemli.org>
2016-03-18 13:15:32 +0100

    allow alpha / beta / rc releases

--- tests/acceptance/build-info-test.js
+++ tests/acceptance/build-info-test.js
@@ -9,7 +9,7 @@ test('version is included as html meta tag', function(assert) {
   andThen(function() {
     assert.ok($('head meta[name="build-info"]').length === 1, 'tag exists');
     assert.ok(
-      $('head meta[name="build-info"]').attr('content').match(/^version=v\d[\d\.]+\d(\+[\da-z]{8})?$/) !== null,
+      $('head meta[name="build-info"]').attr('content').match(/^version=v\d[\d\.]+\d(-(alpha|beta|rc)\d)?(\+[\da-z]{8})?$/) !== null,
       'format '.concat($('head meta[name="build-info"]').attr('content'), ' is correct')
     );
   });
