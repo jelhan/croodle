529c96420b15c7b6e1dab69b760ea1801e2e97ef
jelhan <jeldrik@systemli.org>
2016-05-28 11:32:47 +0200

    Fixes: undefined is not a function (swap) caused by webshim https://github.com/aFarkas/webshim/issues/560

--- app/initializers/webshim-polyfill.js
+++ app/initializers/webshim-polyfill.js
@@ -1,3 +1,4 @@
+import jQuery from 'jquery';
 /* global webshim */
 
 /*
@@ -5,6 +6,31 @@
  * used for input[type='time']
  */
 export function initialize() {
+  /*
+   * Fixes: TypeError: a.swap is not a function
+   * https://github.com/aFarkas/webshim/issues/560#issuecomment-181543832
+   */
+  /* jscs:disable */
+  if (typeof jQuery.swap !== 'function') {
+    jQuery.swap = function( elem, options, callback, args ) {
+      var ret, name, old = {};
+      // Remember the old values, and insert the new ones
+      for ( name in options ) {
+        old[ name ] = elem.style[ name ];
+        elem.style[ name ] = options[ name ];
+      }
+
+      ret = callback.apply( elem, args || [] );
+
+      // Revert the old values
+      for ( name in options ) {
+        elem.style[ name ] = old[ name ];
+      }
+      return ret;
+    };
+  }
+  /* jscs:enable */
+
   webshim.setOptions({
     waitReady: false
   });
