3f84279b6af3a2b12ee4a7b84ab628cd97a10d30
jelhan <jeldrik@systemli.org>
2016-06-02 00:02:28 +0200

    create/options (findADate) label validation state reflect all options

--- app/components/create-options-text.js
+++ app/components/create-options-text.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import BsForm from 'ember-bootstrap/components/bs-form';
 
 export default Ember.Component.extend({
   actions: {
@@ -16,6 +17,66 @@ export default Ember.Component.extend({
       this.get('options').removeAt(position);
     }
   },
+
+  anyElementHasFeedback: Ember.computed('form.childFormElements.@each.hasFeedback', function() {
+    const childFormElements = this.get('form.childFormElements');
+
+    if (childFormElements) {
+      return childFormElements.any((childFormElement) => {
+        return childFormElement.get('hasFeedback');
+      });
+    } else {
+      return false;
+    }
+  }),
+
+  anyElementIsInvalid: Ember.computed('form.childFormElements.@each.validation', function() {
+    const childFormElements = this.get('form.childFormElements');
+
+    if (childFormElements) {
+      return childFormElements.any((childFormElement) => {
+        return childFormElement.get('validation') === 'error';
+      });
+    } else {
+      return false;
+    }
+  }),
+
+  everyElementIsValid: Ember.computed('form.childFormElements.@each.validation', function() {
+    const anyElementIsInvalid = this.get('anyElementIsInvalid');
+    if (anyElementIsInvalid) {
+      return false;
+    }
+
+    // childFormElements contains button wrapper element which should not be taken into account here
+    const childFormElements = this.get('form.childFormElements').filterBy('hasValidator');
+    if (childFormElements) {
+      return childFormElements.every((childFormElement) => {
+        return childFormElement.get('hasFeedback') && childFormElement.get('validation') === 'success';
+      });
+    } else {
+      return false;
+    }
+  }),
+
+  form: null,
+  registerForm: Ember.on('didInsertElement', function() {
+    this.set('form', this.nearestOfType(BsForm));
+  }),
+
+  labelValidationClass: Ember.computed('anyElementHasFeedback', 'anyElementIsInvalid', 'everyElementIsValid', function() {
+    if (!this.get('anyElementHasFeedback')) {
+      return 'label-has-no-validation';
+    } else if (this.get('anyElementIsInvalid')) {
+      return 'label-has-error';
+    } else if (this.get('everyElementIsValid')) {
+      return 'label-has-success';
+    } else {
+      return 'label-has-no-validation';
+    }
+  }),
+
+  classNameBindings: ['labelValidationClass'],
 
   enforceMinimalOptionsAmount: Ember.observer('options', 'isMakeAPoll', function() {
     if (this.get('options.length') < 2) {
