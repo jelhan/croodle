jelhan/croodle

commit 66b89456d3984e5769f8b9479d1279f5a41fa949

Fix: feedback if participation can't be saved
adresses #78

app/controllers/poll/participation.js
@@ -70,12 +70,13 @@ export default Ember.Controller.extend(Validations, {
   actions: {
     submit() {
       if (this.get('validations.isValid')) {
-        const user = this.store.createRecord('user', {
+        const user = this.get('newUserRecord') || this.store.createRecord('user', {
           creationDate: new Date(),
-          name: this.get('name'),
           poll: this.get('pollController.model'),
           version: this.buildInfo.semver
         });
+
+        user.set('name', this.get('name'));
 
         const selections = user.get('selections');
         const possibleAnswers = this.get('pollController.model.answers');
@@ -100,28 +101,28 @@ export default Ember.Controller.extend(Validations, {
           }
         });
 
-        user.save()
-        .catch(() => {
-          // error: new user is not saved
-          this.send('openModal', {
-            template: 'save-retry',
-            model: {
-              record: user
-            }
-          });
-        })
-        .then(() => {
-          // reset form
-          this.set('name', '');
-          this.get('selections').forEach((selection) => {
-            selection.set('value', null);
-          });
-
-          this.transitionToRoute('poll.evaluation', this.get('model'), {
-            queryParams: { encryptionKey: this.get('encryption.key') }
-          });
-        });
+        this.set('newUserRecord', user);
+        this.send('save');
       }
+    },
+    save() {
+      const user = this.get('newUserRecord');
+      user.save()
+      .then(() => {
+        this.set('savingFailed', false);
+
+        // reset form
+        this.set('name', '');
+        this.get('selections').forEach((selection) => {
+          selection.set('value', null);
+        });
+
+        this.transitionToRoute('poll.evaluation', this.get('model'), {
+          queryParams: { encryptionKey: this.get('encryption.key') }
+        });
+      }, () => {
+        this.set('savingFailed', true);
+      });
     }
   },
 
@@ -166,6 +167,8 @@ export default Ember.Controller.extend(Validations, {
       }
     });
   }),
+
+  savingFailed: false,
 
   selections: Ember.computed('pollController.model.options', 'pollController.dates', function() {
     let options;

app/routes/application.js
@@ -1,22 +1,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  actions: {
-    openModal(options) {
-      const modalController = this.controllerFor(`modal/${options.template}`);
-      modalController.set('model', options.model);
-      this.render(`modal/${options.template}`, {
-        into: 'application',
-        outlet: 'modal',
-        controller: modalController
-      });
-    },
-
-    removeModal() {
-      this.disconnectOutlet({
-        outlet: 'modal',
-        parentView: 'application'
-      });
-    }
-  }
-});

app/templates/modal/save-retry.hbs
@@ -1,15 +1,0 @@
-<div class="modal fade" data-backdrop="false" data-keyboard="false">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title">{{t "modal.save-retry.title"}}</h4>
-      </div>
-      <div class="modal-body">
-        <p>{{t "modal.save-retry.text"}}</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" {{action "saveRetry"}}>{{t "modal.save-retry.button-retry"}}</button>
-      </div>
-    </div>
-  </div>
-</div>

app/templates/poll.hbs
@@ -48,7 +48,7 @@
   keyboard=false
   autoClose=false
 }}
-  {{#bs-modal-body}}
+  {{#bs-modal-body classNames='modal-body-choose-timezone'}}
     <p>
       {{t "poll.modal.timezoneDiffers.body"}}
     </p>

app/templates/poll/participation.hbs
@@ -55,3 +55,23 @@
     {{/bs-form-element}}
   {{/bs-form}}
 </div>
+
+{{#bs-modal
+  open=savingFailed
+  title=(t 'modal.save-retry.title')
+  body=false
+  footer=false
+  closeButton=false
+  autoClose=false
+}}
+  {{#bs-modal-body classNames='modal-body-saving-retry'}}
+    <p>{{t "modal.save-retry.text"}}</p>
+  {{/bs-modal-body}}
+  {{#bs-modal-footer}}
+    {{bs-button
+      defaultText=(t 'modal.save-retry.button-retry')
+      type='primary'
+      action='save'
+    }}
+  {{/bs-modal-footer}}
+{{/bs-modal}}

app/views/modal/save-retry.js
@@ -1,11 +1,0 @@
-import Ember from 'ember';
-
-export default Ember.View.extend({
-  show: function() {
-    this.$('.modal').modal();
-
-    this.$('.modal').on('hidden.bs.modal', () => {
-      this.get('controller').send('removeModal');
-    });
-  }.on('didInsertElement')
-});

tests/acceptance/view-poll-test.js
@@ -5,7 +5,7 @@ import Pretender from 'pretender';
 import serverGetPolls from '../helpers/server-get-polls';
 import pageParticipation from 'croodle/tests/pages/poll/participation';
 /* jshint proto: true */
-/* global jstz, moment, start, stop */
+/* global jstz, moment */
 
 let application, server;
 
@@ -134,13 +134,9 @@ test('view a poll while timezone differs from the one poll got created in and ch
   });
 
   visit(`/poll/${id}?encryptionKey=${encryptionKey}`).then(function() {
-    stop();
-    Ember.run.later(function() {
-      start();
-
-      assert.equal(
-        find('.modal').css('display'),
-        'block',
+    Ember.run.next(function() {
+      assert.ok(
+        find('.modal .modal-body-choose-timezone').is(':visible'),
         'user gets asked which timezone should be used'
       );
 
@@ -155,18 +151,14 @@ test('view a poll while timezone differs from the one poll got created in and ch
           ]
         );
 
-        stop();
-        Ember.run.later(function() {
-          start();
-
-          assert.equal(
-            find('.modal').css('display'),
-            'none',
+        Ember.run.next(function() {
+          assert.notOk(
+            find('.modal .modal-body-choose-timezone').is(':visible'),
             'modal is closed'
           );
-        }, 1000);
+        });
       });
-    }, 1000);
+    });
   });
 });
 
@@ -197,13 +189,9 @@ test('view a poll while timezone differs from the one poll got created in and ch
   });
 
   visit(`/poll/${id}?encryptionKey=${encryptionKey}`).then(function() {
-    stop();
-    Ember.run.later(function() {
-      start();
-
-      assert.equal(
-        find('.modal').css('display'),
-        'block',
+    Ember.run.next(function() {
+      assert.ok(
+        find('.modal .modal-body-choose-timezone').is(':visible'),
         'user gets asked which timezone should be used'
       );
 
@@ -218,17 +206,13 @@ test('view a poll while timezone differs from the one poll got created in and ch
           ]
         );
 
-        stop();
-        Ember.run.later(function() {
-          start();
-
-          assert.equal(
-            find('.modal').css('display'),
-            'none',
+        Ember.run.next(function() {
+          assert.notOk(
+            find('.modal .modal-body-choose-timezone').is(':visible'),
             'modal is closed'
           );
-        }, 1000);
+        });
       });
-    }, 1000);
+    });
   });
 });

commit 13969572974b364ab098f4e775b8ae03427603f1

Fix: use a new record per user was broken by 66b89456d3984e5769f8b9479d1279f5a41fa949

app/controllers/poll/participation.js
@@ -72,7 +72,7 @@ export default Controller.extend(Validations, {
   actions: {
     submit() {
       if (this.get('validations.isValid')) {
-        const user = this.get('newUserRecord') || this.store.createRecord('user', {
+        const user = this.store.createRecord('user', {
           creationDate: new Date(),
           poll: this.get('pollController.model'),
           version: this.buildInfo.semver

tests/acceptance/participate-in-a-poll-test.js
@@ -55,6 +55,23 @@ test('participate in a default poll', function(assert) {
       );
       pollHasUsersCount(assert, 1, 'user is added to user selections table');
       pollHasUser(assert, 'Max Meiner', [t('answerTypes.yes.label'), t('answerTypes.no.label')]);
+
+      click('.nav .participation');
+
+      andThen(() => {
+        assert.equal(currentPath(), 'poll.participation');
+        assert.equal(find('.name input').val(), '', 'input for name is cleared');
+        assert.ok(
+          !find('input[type="radio"]').toArray().some((el) => $(el).prop('checked')),
+          'radios are cleared'
+        );
+        pollParticipate('Peter Müller', ['yes', 'yes']);
+
+        andThen(() => {
+          pollHasUsersCount(assert, 2, 'user is added to user selections table');
+          pollHasUser(assert, 'Peter Müller', [t('answerTypes.yes.label'), t('answerTypes.yes.label')]);
+        });
+      });
     });
   });
 });