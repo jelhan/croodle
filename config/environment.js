1dd23ab59b28fb770e8c45a9b337256094880952
rugk <rugk@posteo.de>
2016-09-03 16:26:57 +0200

    Bring dev and stable CSP header in sync
    Uses the most secure combination of these two.

--- config/environment.js
+++ config/environment.js
@@ -19,7 +19,8 @@ module.exports = function(environment) {
       'connect-src': "'self'",
       'img-src': "'self'",
       'style-src': "'self'",
-      'media-src': "'self'"
+      'media-src': "'self'",
+      'referrer': "no-referrer"
     },
 
     EmberENV: {
