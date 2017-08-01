2f1e729e2cc70b60c55650b3524d40fac577a332
jelhan <jeldrik@systemli.org>
2017-08-01 10:26:38 +0200

    Fix: second pretender instance warning

--- tests/helpers/destroy-app.js
+++ tests/helpers/destroy-app.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 const { run } = Ember;
 
 export default function destroyApp(application) {
+  // this is required to fix "second Pretender instance" warnings
+  if (server) {
+    server.shutdown();
+  }
+
   run(application, 'destroy');
 }
