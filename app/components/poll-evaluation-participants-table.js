jelhan/croodle

commit 1df562b352ccd923c020cbae705b1fcdfbadf5dd

refactor {{poll-evaluation-chart}} and {{poll-evaluation-participants-table}} interfaces to make them less complex

app/templates/components/poll-evaluation-participants-table.hbs
@@ -14,9 +14,9 @@
       {{/if}}
       <tr>
         <th>&nbsp;</th>
-        {{#if isFindADate}}
-          {{#each dates as |date|}}
-            <th>
+        {{#each options as |option|}}
+          <th>
+            {{#if isFindADate}}
               {{#if hasTimes}}
                 {{#if date.hasTime}}
                   {{date.formattedTime}}
@@ -24,15 +24,11 @@
               {{else}}
                 {{date.formatted}}
               {{/if}}
-            </th>
-          {{/each}}
-        {{else}}
-          {{#each options as |option|}}
-            <th>
+            {{else}}
               {{option.title}}
-            </th>
-          {{/each}}
-        {{/if}}
+            {{/if}}
+          </th>
+        {{/each}}
       </tr>
     </thead>
 

app/templates/poll/evaluation.hbs
@@ -4,9 +4,7 @@
   <h3>{{t 'poll.evaluation.overview'}}</h3>
   {{poll-evaluation-chart
     answerType=model.answerType
-    dates=dates
-    isFindADate=model.isFindADate
-    pollOptions=model.options
+    options=(if model.isFindADate dates model.options)
     users=model.users
   }}
 {{/if}}
@@ -14,10 +12,9 @@
 <h3>{{t 'poll.evaluation.participantTable'}}</h3>
 {{poll-evaluation-participants-table
   dateGroups=dateGroups
-  dates=dates
   hasTimes=hasTimes
   isFindADate=model.isFindADate
   isFreeText=model.isFreeText
-  options=model.options
+  options=(if model.isFindADate dates model.options)
   sortedUsers=sortedUsers
 }}

app/components/poll-evaluation-chart.js
@@ -20,17 +20,11 @@ const addArrays = function() {
 export default Component.extend({
   i18n: inject.service(),
   type: 'bar',
-  data: computed('users.[]', 'dates.[]', 'dates.@each.formatted', 'pollOptions.[]', 'pollOptions.@each.title', 'i18n.locale', function() {
-    let labels;
-    if (this.get('isFindADate')) {
-      labels = this.get('dates').map((date) => {
-        return get(date, 'formatted');
-      });
-    } else {
-      labels = this.get('pollOptions').map((pollOption) => {
-        return get(pollOption, 'title');
-      });
-    }
+  data: computed('users.[]', 'options.[]', 'options.@each.formatted', 'options.@each.title', 'i18n.locale', function() {
+    let labels = this.get('options').map((option) => {
+      let formatted = get(option, 'formatted');
+      return isPresent(formatted) ? formatted : get(option, 'title');
+    });
 
     let datasets = [];
     const participants = this.get('users.length');

tests/integration/components/poll-evaluation-chart-test.js
@@ -11,7 +11,7 @@ moduleForComponent('poll-evaluation-chart', 'Integration | Component | poll eval
 });
 
 test('it renders', function(assert) {
-  this.set('dates', [
+  this.set('options', [
     Ember.Object.create({
       formatted: 'Thursday, January 1, 2015',
       title: moment('2015-01-01'),
@@ -70,7 +70,6 @@ test('it renders', function(assert) {
       ]
     })
   ]);
-  this.set('isFindADate', true);
-  this.render(hbs`{{poll-evaluation-chart dates=dates answerType=answerType isFindADate=isFindADate users=users}}`);
+  this.render(hbs`{{poll-evaluation-chart options=options answerType=answerType users=users}}`);
   assert.ok(this.$('canvas'), 'it renders a canvas element');
 });

tests/unit/components/poll-evaluation-chart-test.js
@@ -24,7 +24,7 @@ moduleForComponent('poll-evaluation-chart', 'Unit | Component | poll evaluation 
 });
 
 test('data is a valid ChartJS dataset for FindADate', function(assert) {
-  const dates = [
+  let options = [
     Ember.Object.create({
       formatted: 'Thursday, January 1, 2015',
       title: moment('2015-01-01'),
@@ -48,8 +48,7 @@ test('data is a valid ChartJS dataset for FindADate', function(assert) {
   ];
   let component = this.subject({
     answerType: 'YesNoMaybe',
-    dates,
-    isFindADate: true,
+    options,
     users: [
       Ember.Object.create({
         id: 1,
@@ -90,8 +89,8 @@ test('data is a valid ChartJS dataset for FindADate', function(assert) {
   const data = component.get('data');
   assert.deepEqual(
     data.labels,
-    dates.map((date) => {
-      return date.hasTime ? date.title.format('LLLL') : date.title.format(
+    options.map((date) => {
+      return date.get('hasTime') ? date.get('title').format('LLLL') : date.get('title').format(
         moment.localeData()
           .longDateFormat('LLLL')
           .replace(
@@ -140,8 +139,7 @@ test('data is a valid ChartJS dataset for MakeAPoll', function(assert) {
   ];
   let component = this.subject({
     answerType: 'YesNoMaybe',
-    pollOptions: options,
-    isFindADate: false,
+    options,
     users: [
       Ember.Object.create({
         id: 1,

commit a90a97f208735bcbd0003f91d46782b15b741abb

refactor: move dateGroups to {{poll-evaluation-participants-table}} and use array.groupBy macro
dateGroups property of poll controller was only consumed by {{poll-evaluation-participants-table}}
component. So moved it there.
Also refactored property to use `array.groupBy()` macro provided by ember-awesome-macros.

This also fixes a bug introduced by 1df562b352ccd923c020cbae705b1fcdfbadf5dd which results in
missing times in {{poll-evaluation-participants-table}}.

app/components/poll-evaluation-participants-table.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
+import moment from 'moment';
+import { groupBy } from 'ember-awesome-macros/array';
 
-export default Ember.Component.extend({
+const {
+  Component,
+  computed
+} = Ember;
+
+export default Component.extend({
   didInsertElement() {
     this._super();
     Ember.run.scheduleOnce('afterRender', this, function() {
@@ -113,22 +120,21 @@ export default Ember.Component.extend({
     return scrollBarWidth;
   },
 
-  /*
-   * show / hide top scrollbar depending on window position
-   * used as event callback when window is scrolled
-   */
-  updateScrollbarTopVisibility() {
-    const windowTop = Ember.$(window).scrollTop();
-    const tableTop = Ember.$('.table-scroll table').offset().top;
-    if (windowTop >= tableTop - this.getScrollbarHeight()) {
-      Ember.$('.top-scrollbar-floatThead').show();
+  momentLongDayFormat: computed('currentLocale', function() {
+    let currentLocale = this.get('currentLocale');
+    return moment.localeData(currentLocale)
+      .longDateFormat('LLLL')
+      .replace(
+        moment.localeData(currentLocale).longDateFormat('LT'), '')
+      .trim();
+  }),
 
-      // update scroll position
-      Ember.$('.top-scrollbar-floatThead').scrollLeft(Ember.$('.table-scroll').scrollLeft());
-    } else {
-      Ember.$('.top-scrollbar-floatThead').hide();
-    }
-  },
+  optionsGroupedByDates: groupBy('options', 'optionsGroupedBy', function(groupValue, currentValue) {
+    // have to parse the date cause due to timezone it may start with another day string but be at same day due to timezone
+    // e.g. '2015-01-01T23:00:00.000Z' and '2015-01-02T00:00:00.000Z' both are at '2015-01-02' for timezone offset '+01:00'
+    return moment(groupValue).format('YYYY-MM-DD') === moment(currentValue).format('YYYY-MM-DD');
+  }),
+  optionsGroupedBy: 'title',
 
   /*
    * resize scrollbars
@@ -149,6 +155,23 @@ export default Ember.Component.extend({
       this.resizeScrollbars();
     });
   }),
+
+  /*
+   * show / hide top scrollbar depending on window position
+   * used as event callback when window is scrolled
+   */
+  updateScrollbarTopVisibility() {
+    const windowTop = Ember.$(window).scrollTop();
+    const tableTop = Ember.$('.table-scroll table').offset().top;
+    if (windowTop >= tableTop - this.getScrollbarHeight()) {
+      Ember.$('.top-scrollbar-floatThead').show();
+
+      // update scroll position
+      Ember.$('.top-scrollbar-floatThead').scrollLeft(Ember.$('.table-scroll').scrollLeft());
+    } else {
+      Ember.$('.top-scrollbar-floatThead').hide();
+    }
+  },
 
   /*
    * clean up

app/controllers/poll.js
@@ -101,80 +101,6 @@ export default Controller.extend({
   encryptionKey: '',
   queryParams: ['encryptionKey'],
 
-  dateGroups: computed('dates.[]', function() {
-    // group dates only for find a date
-    // and if there is atleast one time
-    if (
-      this.get('model.isFindADate') !== true &&
-      this.get('hasTimes')
-    ) {
-      return [];
-    }
-
-    const datetimes = this.get('dates');
-    const dateGroupObject = EmberObject.extend({
-      colspan: null,
-      formatted: computed('value', 'i18n.locale', function() {
-        const date = this.get('value');
-        const locale = this.get('i18n.locale');
-        const longDateFormat = moment.localeData().longDateFormat('LLLL')
-                                      .replace(
-                                        moment.localeData().longDateFormat('LT'), '')
-                                      .trim();
-
-        // moment caches locale so we have to check if it's changed
-        if (date.locale() !== locale) {
-          date.locale(locale);
-        }
-
-        return date.format(longDateFormat);
-      }),
-      i18n: inject.service(),
-      value: null
-    });
-    // need to inject owner into dateGroupObject to support service injection
-    const owner = getOwner(this);
-    let dateGroups = [];
-
-    let count = 0;
-    let lastDate = null;
-    datetimes.forEach(function(el) {
-      let date = moment(el.get('date'))
-                   .hour(0)
-                   .minute(0)
-                   .seconds(0)
-                   .milliseconds(0);
-
-      if (lastDate === null) {
-        lastDate = date;
-      }
-
-      if (date.valueOf() === lastDate.valueOf()) {
-        count++;
-      } else {
-        // push last values;
-        dateGroups.pushObject(
-          dateGroupObject.create(owner.ownerInjection(), {
-            'value': lastDate,
-            'colspan': count
-          })
-        );
-
-        // set lastDate to current date and reset count
-        lastDate = date;
-        count = 1;
-      }
-    });
-    dateGroups.pushObject(
-      dateGroupObject.create(owner.ownerInjection(), {
-        'value': lastDate,
-        'colspan': count
-      })
-    );
-
-    return dateGroups;
-  }),
-
   /*
    * handles options if they are dates
    */
@@ -202,7 +128,8 @@ export default Controller.extend({
       return false;
     } else {
       return this.get('model.options').any((option) => {
-        return moment(option.get('title'), 'YYYY-MM-DD', true).isValid() === false;
+        let dayStringLength = 10; // 'YYYY-MM-DD'.length
+        return option.get('title').length > dayStringLength;
       });
     }
   }),
@@ -237,5 +164,9 @@ export default Controller.extend({
 
   mustChooseTimezone: computed('timezoneDiffers', 'timezoneChoosen', function() {
     return this.get('timezoneDiffers') && !this.get('timezoneChoosen');
+  }),
+
+  timezone: computed('useLocalTimezone', function() {
+    return this.get('useLocalTimezone') ? undefined : this.get('model.timezone');
   })
 });

app/controllers/poll/evaluation.js
@@ -3,12 +3,22 @@ import Ember from 'ember';
 const { $, computed, Controller, inject } = Ember;
 
 export default Controller.extend({
-  dateGroups: computed.reads('pollController.dateGroups'),
+  currentLocale: computed.reads('i18n.locale'),
+
   dates: computed.reads('pollController.dates'),
+
   hasTimes: computed.reads('pollController.hasTimes'),
+
+  i18n: inject.service(),
+
+  optionsGroupedByDates: computed.reads('pollController.optionsGroupedByDates'),
+
   pollController: inject.controller('poll'),
+
   sortedUsers: computed.sort('pollController.model.users', 'usersSorting'),
   usersSorting: ['creationDate'],
+
+  timezone: computed.reads('pollController.timezone'),
 
   /*
    * evaluates poll data
@@ -59,10 +69,10 @@ export default Controller.extend({
         let answerindex;
 
         // get answer index by lookup array
-        if (typeof lookup[selection.value.label] === 'undefined') {
+        if (typeof lookup[selection.get('value.label')] === 'undefined') {
           answerindex = lookup[null];
         } else {
-          answerindex = lookup[selection.value.label];
+          answerindex = lookup[selection.get('value.label')];
         }
 
         // increment counter

app/templates/components/poll-evaluation-participants-table.hbs
@@ -4,9 +4,14 @@
       {{#if hasTimes}}
         <tr class="dateGroups">
           <th>&nbsp;</th>
-          {{#each dateGroups as |dateGroup|}}
-            <th colspan="{{dateGroup.colspan}}">
-              {{dateGroup.formatted}}
+          {{#each optionsGroupedByDates as |optionGroup|}}
+            <th colspan="{{optionGroup.items.length}}">
+              {{moment-format
+                optionGroup.value
+                momentLongDayFormat
+                locale=currentLocale
+                timeZone=timezone
+              }}
             </th>
           {{/each}}
           <th>&nbsp;</th>
@@ -18,11 +23,11 @@
           <th>
             {{#if isFindADate}}
               {{#if hasTimes}}
-                {{#if date.hasTime}}
-                  {{date.formattedTime}}
+                {{#if option.hasTime}}
+                  {{option.formattedTime}}
                 {{/if}}
               {{else}}
-                {{date.formatted}}
+                {{option.formatted}}
               {{/if}}
             {{else}}
               {{option.title}}

app/templates/poll/evaluation.hbs
@@ -11,10 +11,11 @@
 
 <h3>{{t 'poll.evaluation.participantTable'}}</h3>
 {{poll-evaluation-participants-table
-  dateGroups=dateGroups
+  currentLocale=currentLocale
   hasTimes=hasTimes
   isFindADate=model.isFindADate
   isFreeText=model.isFreeText
   options=(if model.isFindADate dates model.options)
   sortedUsers=sortedUsers
+  timezone=timezone
 }}