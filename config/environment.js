jelhan/croodle

commit 632c36eb127641732e704f4d7127fe833ce72b9f

updates dependencies and fixes property modified twice in a single render deprecations

config/environment.js
@@ -1,5 +1,5 @@
-/* jshint node: true */
-/* jscs:disable requireEnhancedObjectLiterals */
+/* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
   const ENV = {

.eslintrc.js
@@ -1,0 +1,14 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  extends: 'eslint:recommended',
+  env: {
+    browser: true,
+    es6: true
+  },
+  rules: {
+  }
+};

.gitignore
@@ -1,4 +1,4 @@
-# See http://help.github.com/ignore-files/ for more about ignoring files.
+# See https://help.github.com/ignore-files/ for more about ignoring files.
 
 # compiled output
 /dist
@@ -18,4 +18,10 @@
 /coverage/*
 /libpeerconnection.log
 npm-debug.log*
+yarn-error.log
 testem.log
+
+# ember-try
+.node_modules.ember-try/
+bower.json.ember-try
+package.json.ember-try

.jshintrc
@@ -1,32 +1,0 @@
-{
-  "predef": [
-    "document",
-    "window",
-    "-Promise"
-  ],
-  "browser": true,
-  "boss": true,
-  "curly": true,
-  "debug": false,
-  "devel": true,
-  "eqeqeq": true,
-  "evil": true,
-  "forin": false,
-  "immed": false,
-  "laxbreak": false,
-  "newcap": true,
-  "noarg": true,
-  "noempty": false,
-  "nonew": false,
-  "nomen": false,
-  "onevar": false,
-  "plusplus": false,
-  "regexp": false,
-  "undef": true,
-  "sub": true,
-  "strict": false,
-  "white": false,
-  "eqnull": true,
-  "esversion": 6,
-  "unused": true
-}

.travis.yml
@@ -16,26 +16,16 @@ sudo: false
 
 cache:
   yarn: true
-  directories:
-    - travis_phantomjs
-
-addons:
-  apt:
-    sources:
-      - sourceline: 'deb https://dl.yarnpkg.com/debian/ stable main'
-        key_url: 'https://dl.yarnpkg.com/debian/pubkey.gpg'
-    packages:
-      - yarn
 
 before_install:
-  # update node
-  - nvm install 6
-  # update phantomjs
-  - export PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH; if [ $(phantomjs --version) != '2.1.1' ]; then rm -rf $PWD/travis_phantomjs; mkdir -p $PWD/travis_phantomjs && wget https://assets.membergetmember.co/software/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 && tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs; fi
-
+  - if $TEST_EMBER || $TEST_SAUCHE; then curl -o- -L https://yarnpkg.com/install.sh | bash; fi
+  - if $TEST_EMBER || $TEST_SAUCHE; then export PATH=$HOME/.yarn/bin:$PATH; fi
+  - if $TEST_EMBER; then yarn global add phantomjs-prebuilt; fi
+  - if $TEST_EMBER; then phantomjs --version; fi
 install:
   - if $TEST_EMBER || $TEST_SAUCE; then yarn global add bower; fi
-  - if $TEST_EMBER || $TEST_SAUCE; then yarn install; fi
+  - if $TEST_EMBER || $TEST_SAUCE; then yarn global add bower; fi
+  - if $TEST_EMBER || $TEST_SAUCE; then yarn install --no-interactive; fi
   - if $TEST_EMBER || $TEST_SAUCE; then bower install; fi
   # install non development composer requirements for api
   - if $TEST_EMBER || $TEST_SAUCE; then cd api/ && composer install --no-dev && cd ..; fi

app/app.js
@@ -7,8 +7,6 @@ const { Application } = Ember;
 
 let App;
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
 App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,

app/components/create-options-datetime.js
@@ -6,7 +6,7 @@ from 'ember-cp-validations';
 import { groupBy } from 'ember-array-computed-macros';
 import Form from 'ember-bootstrap/components/bs-form';
 
-const { computed, Component, inject, isEmpty, isPresent, observer } = Ember;
+const { computed, Component, inject, isArray, isEmpty, isPresent, observer } = Ember;
 const { filter, mapBy, readOnly } = computed;
 
 let modelValidations = buildValidations({
@@ -147,7 +147,10 @@ export default Component.extend(modelValidations, {
       return childView instanceof Form;
     });
   }),
-  formElements: readOnly('form.childFormElements'),
+  formElements: computed('form.childFormElements', function() {
+    let formElements = this.get('form.childFormElements');
+    return isArray(formElements) ? formElements : [];
+  }),
   daysValidationState: computed('formElements.@each.validation', function() {
     return this.get('formElements').reduce(function(daysValidationState, item) {
       const day = item.get('model.day');

app/components/create-options-text.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import BsForm from 'ember-bootstrap/components/bs-form';
 import { anyBy } from 'ember-array-computed-macros';
 
-const { Component, computed, inject, observer, on } = Ember;
+const { Component, computed, inject, observer, on, run } = Ember;
 
 export default Component.extend({
   actions: {
@@ -49,21 +49,24 @@ export default Component.extend({
     this.set('form', this.nearestOfType(BsForm));
   }),
 
-  labelValidationClass: computed('anyElementHasFeedback', 'anyElementIsInvalid', 'everyElementIsValid', function() {
-    if (!this.get('anyElementHasFeedback')) {
-      return 'label-has-no-validation';
-    }
+  labelValidationClass: 'label-has-no-validation',
+  updateLabelValidationClass: observer('anyElementHasFeedback', 'anyElementIsInvalid', 'everyElementIsValid', function() {
+    run.scheduleOnce('sync', () => {
+      let validationClass;
 
-    if (this.get('anyElementIsInvalid')) {
-      return 'label-has-error';
-    }
+      if (!this.get('anyElementHasFeedback')) {
+        validationClass = 'label-has-no-validation';
+      } else if (this.get('anyElementIsInvalid')) {
+        validationClass = 'label-has-error';
+      } else if (this.get('everyElementIsValid')) {
+        validationClass = 'label-has-success';
+      } else {
+        validationClass = 'label-has-no-validation';
+      }
 
-    if (this.get('everyElementIsValid')) {
-      return 'label-has-success';
-    }
-
-    return 'label-has-no-validation';
-  }),
+      this.set('labelValidationClass', validationClass);
+    });
+  }).on('init'),
 
   classNameBindings: ['labelValidationClass'],
 

commit 2ac3bfacdb85724b3f31b634d55d0d455dc811f1

Fix: do not link assets with absolut paths to support serving from subdirectory
Bug introduced by 632c36eb127641732e704f4d7127fe833ce72b9f

config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     modulePrefix: 'croodle',
     environment: environment,
     locationType: 'hash',
-    rootURL: '/',
+    rootURL: '',
 
     APP: {
       // Here you can pass flags/options to your application instance
@@ -44,6 +44,8 @@ module.exports = function(environment) {
   };
 
   if (environment === 'test') {
+    ENV.rootURL = '/';
+
     // Testem prefers this...
     ENV.locationType = 'hash';
 