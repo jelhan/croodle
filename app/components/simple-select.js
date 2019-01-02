e22acf66988a27581cd19b61ca31d5ed65a2fad2
Jeldrik Hanschke <admin@jhanschke.de>
2019-01-02 17:59:53 +0100

    refactor: drop ember-simple-select

--- app/components/simple-select.js
+++
@@ -1,6 +0,0 @@
-import SimpleSelect from 'ember-simple-select/components/simple-select';
-import AutofocusSupport from 'croodle/mixins/autofocus-support';
-
-export default SimpleSelect.reopen({
-  classNames: ['form-control']
-}).extend(AutofocusSupport);
--- app/controllers/create/index.js
+++ app/controllers/create/index.js
@@ -1,8 +1,6 @@
 import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
 import Controller from '@ember/controller';
-import { getOwner } from '@ember/application';
-import EmberObject, { computed } from '@ember/object';
 import {
   validator, buildValidations
 }
@@ -19,14 +17,6 @@ const Validations = buildValidations({
       dependentKeys: ['model.i18n.locale']
     })
   ]
-});
-
-const TranslateableObject = EmberObject.extend({
-  i18n: service(),
-  label: computed('labelTranslation', 'i18n.locale', function() {
-    return this.i18n.t(this.labelTranslation);
-  }),
-  labelTranslation: undefined
 });
 
 export default Controller.extend(Validations, {
@@ -46,20 +36,5 @@ export default Controller.extend(Validations, {
     this.get('i18n.locale');
   },
 
-  pollType: alias('model.pollType'),
-
-  pollTypes: computed('', function() {
-    const owner = getOwner(this);
-
-    return [
-      TranslateableObject.create(owner.ownerInjection(), {
-        id: 'FindADate',
-        labelTranslation: 'pollTypes.findADate.label'
-      }),
-      TranslateableObject.create(owner.ownerInjection(), {
-        id: 'MakeAPoll',
-        labelTranslation: 'pollTypes.makeAPoll.label'
-      })
-    ];
-  })
+  pollType: alias('model.pollType')
 });
