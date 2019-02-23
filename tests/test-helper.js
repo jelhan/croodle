ac96aaf04414f0b46c9a7a43e8f325fe22e09f9e
Jeldrik Hanschke <admin@jhanschke.de>
2019-02-23 22:43:53 +0100

    tests should fail if violating CSP

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,6 +3,14 @@ import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
+document.addEventListener('securitypolicyviolation', function({ blockedURI, violatedDirective }) {
+  throw new Error(
+    'Content-Security-Policy violation detected: ' +
+    `Violated directive: ${violatedDirective}. ` +
+    `Blocked URI: ${blockedURI}`
+  );
+});
+
 setApplication(Application.create(config.APP));
 
 start();
