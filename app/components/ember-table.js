43e5079c06c90a19496bc3a467711e34d2b3f286
Jeldrik Hanschke <admin@jhanschke.de>
2019-04-19 01:09:32 +0200

    remove left-over from ember-table experiment

--- app/components/ember-table.js
+++
@@ -1,9 +0,0 @@
-import BaseTable from 'ember-table/components/ember-table/component';
-
-export default class EmberTable extends BaseTable {
-  didInsertElement() {
-    super.didInsertElement(...arguments);
-
-    this.element.querySelector('table').classList.add('table');
-  }
-}
