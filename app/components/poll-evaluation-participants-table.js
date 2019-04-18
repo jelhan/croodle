430484da9f72a4b5e3d9f034bce7c1533c3e849c
Jeldrik Hanschke <admin@jhanschke.de>
2019-04-19 00:21:04 +0200

    refactor to use a plain table again

--- app/components/poll-evaluation-participants-table.js
+++ app/components/poll-evaluation-participants-table.js
@@ -1,55 +1,12 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
 import { raw } from 'ember-awesome-macros';
 import { groupBy, sort } from 'ember-awesome-macros/array';
 
 export default Component.extend({
-  columns: computed('optionsGroupedByDays.[]', function() {
-    let columns = [
-      {
-        isFixed: 'left',
-        name: '',
-        valuePath: 'name',
-      }
-    ];
-
-    if (!this.hasTimes) {
-      return columns.concat(
-        this.options.map(({ title }, index) => {
-          return {
-            isAnswerCell: true,
-            index,
-            momentFormat: this.isFindADate ? this.momentLongDayFormat : null,
-            name: title,
-          };
-        })
-      );
-    }
-
-    let index = 0;
-    return columns.concat(
-      this.optionsGroupedByDays.map(({ value, items }) => {
-        return {
-          momentFormat: this.momentLongDayFormat,
-          name: value,
-          subcolumns: items.map(({ date, hasTime }) => {
-            let subcolumn = {
-              isAnswerCell: true,
-              index,
-              momentFormat: hasTime ? 'LT' : null,
-              name: hasTime ? date : '',
-            };
-            index++;
-            return subcolumn;
-          }),
-        };
-      })
-    );
-  }),
-
   hasTimes: readOnly('poll.hasTimes'),
 
+  isFindADate: readOnly('poll.isFindADate'),
   isFreeText: readOnly('poll.isFreeText'),
 
   options: readOnly('poll.options'),
