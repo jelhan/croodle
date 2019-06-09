0adee3f162c094d70b02ee05af71367aeacf874b
Jeldrik Hanschke <admin@jhanschke.de>
2019-06-09 12:34:37 +0200

    refactor form navigation buttons

---
+++ tests/integration/components/next-button-test.js
@@ -0,0 +1,24 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | next-button', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders a button', async function(assert) {
+    await render(hbs`<NextButton />`);
+
+    assert.dom('button').exists();
+  });
+
+  test('it supports block mode', async function(assert) {
+    await render(hbs`
+      <NextButton>
+        some text
+      </NextButton>
+    `);
+
+    assert.dom('button').hasText('some text');
+  });
+});
