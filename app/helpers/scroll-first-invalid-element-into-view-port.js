1203e00b019ab9cdd566a4632a17515c6cc7d9e8
Jeldrik Hanschke <admin@jhanschke.de>
2019-11-09 12:51:15 +0100

    firefox is not scrolling up enough if it's triggered to early

--- app/helpers/scroll-first-invalid-element-into-view-port.js
+++ app/helpers/scroll-first-invalid-element-into-view-port.js
@@ -1,15 +1,19 @@
 import { helper } from '@ember/component/helper';
-import { schedule } from '@ember/runloop';
+import { next } from '@ember/runloop';
 import { assert } from '@ember/debug';
 
 function scrollFirstInvalidElementIntoViewPort() {
-  // focus first invalid control
-  schedule('afterRender', function() {
+  // `schedule('afterRender', function() {})` would be more approperiate but there seems to be a
+  // timing issue in Firefox causing the Browser not scrolling up far enough if doing so
+  // delaying to next runloop therefore
+  next(function() {
     let invalidInput = document.querySelector('.form-control.is-invalid');
     assert(
       'Atleast one form control must be marked as invalid if form submission was rejected as invalid',
       invalidInput
     );
+
+    // focus first invalid control
     invalidInput.focus({ preventScroll: true });
 
     // scroll to label of first invalid control
