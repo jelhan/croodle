27cf1da80ca4e1ca1d318cab40f56ed7c483ac9d
Jeldrik Hanschke <admin@jhanschke.de>
2020-04-08 23:11:50 +0200

    remove IE11 support

--- config/targets.js
+++ config/targets.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions'
+  'last 2 Chrome versions',
+  // Last Edge versions are based on Chrome but not shipped to a significant
+  // number of users. Change to `last 2 Edge versions` as soon as Chrome-based
+  // Edge is shipped to all users.
+  'Edge >= 18',
+  'last 2 Firefox versions',
+  'Firefox ESR',
+  'last 2 Safari versions',
 ];
-
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
 
 module.exports = {
   browsers
---
+++ public/assets/internet-explorer-11-deprecation.js
@@ -0,0 +1,30 @@
+var outerContainer = document.createElement('div');
+outerContainer.style.display = 'flex';
+outerContainer.style.height = '100vh';
+
+var container = document.createElement('div');
+container.style.alignSelf = 'center';
+container.style.margin = 'auto';
+container.style.maxWidth = '50rem';
+container.style.textAlign = 'center';
+outerContainer.appendChild(container);
+
+var header = document.createElement('h2');
+header.style.color = '#B33A3A';
+
+var icon = document.createElement('span');
+icon.className = 'oi oi-warning';
+header.appendChild(icon);
+header.appendChild(
+  document.createTextNode('Your Browser is not supported!')
+);
+header.appendChild(icon.cloneNode());
+container.appendChild(header);
+
+var text = document.createElement('p');
+text.innerText =
+  'You are using an out-dated browser like Internet Explorer 11 that is not supported anymore by Croodle. ' +
+  'Please switch to a modern browser like Firefox, Google Chrome, Microsoft Edge or Safari.';
+container.appendChild(text);
+
+document.body.appendChild(outerContainer);
