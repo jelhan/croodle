1ec8edec9267b706428cad488a7831202933e720
Jeldrik Hanschke <admin@jhanschke.de>
2023-04-15 11:46:23 +0200

    use Glint

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,17 @@
+import { HelperLike } from '@glint/template';
+import { TemplateFactory } from 'ember-cli-htmlbars';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    'page-title': HelperLike<{
+      Args: { Positional: [title: string] };
+      Return: void;
+    }>;
+  }
+}
+
 // Types for compiled templates
 declare module 'croodle/templates/*' {
-  import { TemplateFactory } from 'ember-cli-htmlbars';
-
   const tmpl: TemplateFactory;
   export default tmpl;
 }
