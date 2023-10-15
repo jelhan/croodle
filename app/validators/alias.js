374d168ee906416a1ac7844db255b660e4e7a74c
Jeldrik Hanschke <admin@jhanschke.de>
2023-10-15 14:17:25 +0200

    remove unused leftovers from ember-cp-validations

--- app/validators/alias.js
+++
@@ -1,9 +0,0 @@
-import classic from 'ember-classic-decorator';
-import Alias from 'ember-cp-validations/validators/alias';
-
-@classic
-export default class AliasValidator extends Alias {
-  validate(value, options, model, attribute) {
-    return super.validate(value, options, model, attribute) || true;
-  }
-}
