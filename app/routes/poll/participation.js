623e0b5d30c79841e152f1c8a8114f5c305ae077
Jeldrik Hanschke <admin@jhanschke.de>
2023-10-15 14:03:39 +0200

    refactor poll participation to not use ember-cp-validations

--- app/routes/poll/participation.js
+++ app/routes/poll/participation.js
@@ -1,9 +1,84 @@
-import classic from 'ember-classic-decorator';
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
+import { tracked } from "@glimmer/tracking";
+import { TrackedArray } from "tracked-built-ins";
+import IntlMessage from "../../utils/intl-message";
 
-@classic
+class FormDataSelections {
+  @tracked value = null;
+  valueIsRequired;
+
+  get valueValidation() {
+    const { value, valueIsRequired } = this;
+
+    if (!value && valueIsRequired) {
+      return new IntlMessage("poll.error.selection.valueMissing");
+    }
+
+    return null;
+  }
+
+  get isValid() {
+    return this.valueValidation === null;
+  }
+
+  constructor(valueIsRequired) {
+    this.valueIsRequired = valueIsRequired;
+  }
+}
+
+class FormData {
+  @tracked name;
+  nameIsRequired;
+  namesTaken;
+  selections;
+
+  get nameValidation() {
+    const { name, nameIsRequired, namesTaken } = this;
+
+    if (!name && nameIsRequired) {
+      return new IntlMessage("poll.error.name.valueMissing");
+    }
+
+    // TODO: Validate that name is unique for this poll
+    if (namesTaken.includes(name)) {
+      return new IntlMessage("poll.error.name.duplicate");
+    }
+
+    return null;
+  }
+
+  get selectionsValidation() {
+    const isValid = this.selections.every((selection) => selection.isValid);
+
+    if (!isValid) {
+      return new IntlMessage('poll.error.newUser.everyOptionIsAnswered');
+    }
+
+    return null;
+  }
+
+  constructor(options, { nameIsRequired, namesTaken, selectionIsRequired }) {
+    this.nameIsRequired = nameIsRequired;
+    this.namesTaken = namesTaken;
+    this.selections = new TrackedArray(
+      options.map(() => new FormDataSelections(selectionIsRequired))
+    );
+  }
+}
+
 export default class ParticipationRoute extends Route {
   model() {
-    return this.modelFor('poll');
+    const poll = this.modelFor("poll");
+    const { anonymousUser, forceAnswer, options, users } = poll;
+    const formData = new FormData(options, {
+      nameIsRequired: !anonymousUser,
+      namesTaken: users.map(({ name }) => name),
+      selectionIsRequired: forceAnswer,
+    });
+
+    return {
+      formData,
+      poll,
+    };
   }
 }
