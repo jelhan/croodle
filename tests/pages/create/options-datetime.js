2eb47870b08106a163fff643c5337ffcabb54d15
Jeldrik Hanschke <admin@jhanschke.de>
2023-10-16 00:00:52 +0200

    upgrade to new APIs

--- tests/pages/create/options-datetime.js
+++ tests/pages/create/options-datetime.js
@@ -8,15 +8,12 @@ let { clickable, collection, fillable, hasClass, text } = PageObject;
 
 export default PageObject.create(
   assign({}, defaultsForCreate, {
-    times: collection({
-      itemScope: '.form-group',
-      item: {
-        add: clickable('button.add'),
-        delete: clickable('button.delete'),
-        label: text('label'),
-        labelIsHidden: hasClass('label', 'sr-only'),
-        time: fillable('input'),
-      },
+    times: collection('.form-group', {
+      add: clickable('button.add'),
+      delete: clickable('button.delete'),
+      label: text('label'),
+      labelIsHidden: hasClass('label', 'sr-only'),
+      time: fillable('input'),
     }),
     firstTime: {
       scope: '.form-group:first',
