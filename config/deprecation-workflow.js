c2549b01d7b4c7885ae14f92c77baa5dc9ca5f4f
Jeldrik Hanschke <admin@jhanschke.de>
2023-10-15 20:45:51 +0200

    get deprecation workflow up to date

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,9 +1,27 @@
-/* global self */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
+  throwOnUnhandled: true,
   workflow: [
-    { handler: 'silence', matchId: 'ember-cli-page-object.old-collection-api' },
-    { handler: 'silence', matchId: 'deprecate-fetch-ember-data-support' },
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    { handler: 'silence', matchId: 'implicit-injections' },
+    {
+      handler: 'silence',
+      matchId: 'ember-cli-mirage-config-routes-only-export',
+    },
+    {
+      handler: 'silence',
+      matchId: 'argument-less-helper-paren-less-invocation',
+    },
+    { handler: 'silence', matchId: 'ember-modifier.function-based-options' },
+    { handler: 'silence', matchId: 'ember-cli-mirage.miragejs.import' },
+    { handler: 'silence', matchId: 'routing.transition-methods' },
+    { handler: 'silence', matchId: 'ember.link-to.tag-name' },
+    { handler: 'silence', matchId: 'ember-cli-page-object.multiple' },
+    { handler: 'silence', matchId: 'autotracking.mutation-after-consumption' },
     { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
   ],
 };
