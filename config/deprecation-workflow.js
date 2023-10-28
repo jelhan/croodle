257e0cf781e19ae98471b53ad089920c9b5b338c
Jeldrik Hanschke <admin@jhanschke.de>
2023-10-28 19:09:02 +0200

    clean-up deprecation workflow

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,22 +2,7 @@
 
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  throwOnUnhandled: true,
   workflow: [
-    {
-      handler: 'throw',
-      matchId: 'deprecated-run-loop-and-computed-dot-access',
-    },
-    {
-      handler: 'throw',
-      matchId: 'ember-cli-mirage-config-routes-only-export',
-    },
-    { handler: 'throw', matchId: 'ember-modifier.function-based-options' },
-    { handler: 'throw', matchId: 'ember-cli-mirage.miragejs.import' },
-    { handler: 'throw', matchId: 'ember.link-to.tag-name' },
-    { handler: 'throw', matchId: 'ember-cli-page-object.multiple' },
-    { handler: 'silence', matchId: 'autotracking.mutation-after-consumption' },
-    { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' },
     // @ember/string deprecation is thrown even if all deprecated behavior is
     // fixed due to a bug in Ember itself. Details can be found in:
     // https://github.com/emberjs/ember.js/issues/20377
