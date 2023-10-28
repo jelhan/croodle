04c0f9e1dcf992665b4fa4b29db80ea272e7193f
Jeldrik Hanschke <admin@jhanschke.de>
2023-10-28 19:33:57 +0200

    drop ember-classic-decorator

--- app/routes/create/options-datetime.js
+++ app/routes/create/options-datetime.js
@@ -1,7 +1,5 @@
-import classic from 'ember-classic-decorator';
 import Route from '@ember/routing/route';
 
-@classic
 export default class OptionsDatetimeRoute extends Route {
   model() {
     return this.modelFor('create');
