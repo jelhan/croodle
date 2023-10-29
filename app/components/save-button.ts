099f25efd505f88a1cad9d26536e247a22b42611
Jeldrik Hanschke <admin@jhanschke.de>
2023-10-29 23:28:48 +0100

    convert another component

---
+++ app/components/save-button.ts
@@ -0,0 +1,20 @@
+import templateOnlyComponent from '@ember/component/template-only';
+
+interface SaveButtonSignature {
+  Args: {
+    Named: {
+      isPending: true;
+    };
+  };
+  Element: HTMLButtonElement;
+}
+
+const SaveButton = templateOnlyComponent<SaveButtonSignature>();
+
+export default SaveButton;
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    SaveButton: typeof SaveButton;
+  }
+}
