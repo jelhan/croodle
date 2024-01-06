e3789c5a94b93725361632abe48946014a39441e
Jeldrik Hanschke <admin@jhanschke.de>
2024-01-06 15:24:30 +0100

    fix CSS bundle size calculation

--- config/bundlesize.js
+++ config/bundlesize.js
@@ -8,7 +8,14 @@ module.exports = {
       compression: 'gzip',
     },
     css: {
-      pattern: 'assets/*.css',
+      // Embroider build includes both the minified and the unminified CSS
+      // `assets/` folder. Only the minified CSS is referenced by `index.html`.
+      // The unminified version does not increase the bundle size for consumers.
+      // We need to exclude it when calculating bundle size.
+      // Only the minified version includes a fingerprint. We can exclude the
+      // unminified version by using a pattern, which only matches files
+      // including a fingerprint hash in their file name.
+      pattern: 'assets/croodle.*.css',
       limit: '16.5KB',
       compression: 'gzip',
     },
