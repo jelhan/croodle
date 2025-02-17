8bb94bcc70c784cee35ac92577d63df960eb4b50
Jeldrik Hanschke <admin@jhanschke.de>
2025-02-17 22:03:01 +0100

    update hbs import to recent best practices

--- tests/integration/components/create-options-datetime-test.js
+++ tests/integration/components/create-options-datetime-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, click, findAll } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 import { setupIntl } from 'ember-intl/test-support';
 
 module('Integration | Component | create options datetime', function (hooks) {
