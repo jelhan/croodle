b35cc06d3bd11696fdd6ef42e31a65a6e249e970
Jeldrik Hanschke <jelhan@users.noreply.github.com>
2025-03-01 11:04:04 +0100

    refactor poll evaluation controller to component (#1089)

---
+++ app/components/poll-evaluation.ts
@@ -0,0 +1,43 @@
+import { service } from '@ember/service';
+import Component from '@glimmer/component';
+import type IntlService from 'ember-intl/services/intl';
+import type { PollEvaluationRouteModel } from 'croodle/routes/poll/evaluation';
+import type PollSettingsService from 'croodle/services/poll-settings';
+
+export interface PollEvaluationSignature {
+  // The arguments accepted by the component
+  Args: {
+    poll: PollEvaluationRouteModel;
+  };
+  // Any blocks yielded by the component
+  Blocks: {
+    default: [];
+  };
+  // The element to which `...attributes` is applied in the component template
+  Element: null;
+}
+
+export default class PollEvaluation extends Component<PollEvaluationSignature> {
+  @service declare intl: IntlService;
+  @service('poll-settings') declare pollSettingsService: PollSettingsService;
+
+  get isEvaluable() {
+    const { poll } = this.args;
+    const { isFreeText, users } = poll;
+    const hasUsers = users.length > 0;
+
+    return hasUsers && !isFreeText;
+  }
+
+  get pollSettings() {
+    const { poll } = this.args;
+
+    return this.pollSettingsService.getSettings(poll);
+  }
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    PollEvaluation: typeof PollEvaluation;
+  }
+}
