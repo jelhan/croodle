86cc8b8c6b456ba8de7c6abaa4f45c3fbba40336
Jeldrik Hanschke <admin@jhanschke.de>
2025-04-01 13:17:21 +0200

    fix testem and CI setup for both Safari and Firefox

--- testem.js
+++ testem.js
@@ -19,29 +19,5 @@ module.exports = {
         '--window-size=1440,900',
       ].filter(Boolean),
     },
-    Firefox: {
-      ci: ['--headless', '--window-size=1440,900'],
-    },
-  },
-  proxies: {
-    '/': {
-      target: 'http://localhost:4200',
-      onlyContentTypes: ['json'],
-    },
-  },
-  launchers: {
-    // Allow safari to proceed without user intervention. See https://github.com/testem/testem/issues/1387
-    Safari: {
-      protocol: 'browser',
-      exe: 'osascript',
-      args: [
-        '-e',
-        `tell application "Safari"
-          activate
-          open location "<url>"
-         end tell
-         delay 3000`,
-      ],
-    },
   },
 };
