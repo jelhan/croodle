947949a40465274591f6227be58e520df0ca3455
Jeldrik Hanschke <admin@jhanschke.de>
2025-11-10 23:23:23 +0100

    drop ember-composable-helpers

---
+++ client/tests/integration/helpers/pick-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'croodle/tests/helpers';
+import { fillIn, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | pick', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('can be used with on helper', async function (assert) {
+    this.set('onChange', (value) => {
+      assert.strictEqual(value, 'some input');
+    });
+
+    await render(hbs`
+      {{!-- template-lint-disable require-input-label --}}
+      <input {{on "change" (pick "target.value" this.onChange)}}>
+    `);
+    await fillIn('input', 'some input');
+  });
+});
