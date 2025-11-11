eed114f7b04c1f7b12ba9469836f5d89c52df3ea
Jeldrik Hanschke <admin@jhanschke.de>
2025-11-11 23:06:16 +0100

    align closer with MirageJS docs

--- client/app/mirage/server.js
+++ client/app/mirage/server.js
@@ -19,18 +19,16 @@ export default function (config) {
       singularize,
     },
 
-    routes,
+    routes() {
+      this.logging = true;
+      this.namespace = '/api/index.php'; // make this `api`, for example, if your API is namespaced
+      this.timing = 400; // delay for each request, automatically set to 0 during testing
+
+      this.get('/polls/:id');
+      this.post('/polls');
+      this.post('/users');
+    },
   };
 
   return createServer(finalConfig);
-}
-
-function routes() {
-  this.logging = true;
-  this.namespace = '/api/index.php'; // make this `api`, for example, if your API is namespaced
-  this.timing = 400; // delay for each request, automatically set to 0 during testing
-
-  this.get('/polls/:id');
-  this.post('/polls');
-  this.post('/users');
 }
